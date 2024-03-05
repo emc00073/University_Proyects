61a032883c3200c1941b3f98a2cc246b2dd67f82
Emilio Martínez Conchillo <124682801+emc00073@users.noreply.github.com>
2024-03-05 18:12:42 +0100

    Try to implement aceleration

--- Animacion-3D/Pr1/Assets/Render_Bezier.cs
+++ Animacion-3D/Pr1/Assets/Render_Bezier.cs
@@ -7,7 +7,12 @@ public class Render_Bezier : MonoBehaviour
     public Transform point0, point1, point2, point3, slider;
     public int numPoints = 50;
     public Transform sphere;
+    public bool renderSphereLines = true;
 
+    public float V0 = 0.2f;
+    public float Ta = 0.4f;
+    public float Tda = 0.8f;
+    public float d = 0.0f;
     
     
     // Add a button to start an animation to move the cube along the path
@@ -16,17 +21,43 @@ public class Render_Bezier : MonoBehaviour
         StartCoroutine(MoveCube());
     }
 
-    // Coroutine to move the cube along the path
+    // Coroutine to move the cube along the path with fade in and fade out animation
     IEnumerator MoveCube()
     {
-        for (float t = 0; t <= 1; t += 1f / numPoints)
+        Time time = new Time();
+        float t = 0;
+
+        while (d < 1)
         {
-            Vector3 pos = Mathf.Pow(1 - t, 3) * point0.position +
-                          3 * Mathf.Pow(1 - t, 2) * t * point1.position +
-                          3 * (1 - t) * Mathf.Pow(t, 2) * point2.position +
-                          Mathf.Pow(t, 3) * point3.position;
-            slider.position = pos;
-            yield return new WaitForSeconds(0.05f);
+            t += Time.deltaTime;
+
+            if (d < Ta) {
+                // Acceleration
+                d = V0 * (t * t) / (2.0f * Ta);
+            }else if(d < Tda) {
+                // Constant speed
+                d = (V0 * Ta / 2.0f) + (V0 * (t - Ta));
+            }
+            else {
+                // Deceleration
+                d = (V0 * Ta / 2.0f) + (V0 * (Tda - Ta)) + (V0 - ((V0 * (t - Tda)) / (1 - Tda)) / 2) * (t - Tda);
+            }
+
+            float x = Mathf.Pow(1 - d, 3) * point0.position.x +
+                      3 * Mathf.Pow(1 - d, 2) * d * point1.position.x +
+                      3 * (1 - d) * Mathf.Pow(d, 2) * point2.position.x +
+                      Mathf.Pow(d, 3) * point3.position.x;
+            float y = Mathf.Pow(1 - d, 3) * point0.position.y +
+                      3 * Mathf.Pow(1 - d, 2) * d * point1.position.y +
+                      3 * (1 - d) * Mathf.Pow(d, 2) * point2.position.y +
+                      Mathf.Pow(d, 3) * point3.position.y;
+            float z = Mathf.Pow(1 - d, 3) * point0.position.z + 
+                      3 * Mathf.Pow(1 - d, 2) * d * point1.position.z + 
+                      3 * (1 - d) * Mathf.Pow(d, 2) * point2.position.z + 
+                      Mathf.Pow(d, 3) * point3.position.z;
+
+            slider.position = new Vector3(x, y, z);
+            yield return null;
         }
     }
 
@@ -68,8 +99,11 @@ public class Render_Bezier : MonoBehaviour
                               3 * (1 - t) * Mathf.Pow(t, 2) * point2.position +
                               Mathf.Pow(t, 3) * point3.position;
 
-                // Create a new sphere at the position
-                Instantiate(sphere, pos, Quaternion.identity).tag = "Sphere";
+                // Create a new sphere at the position and render it if renderSphereLines is true
+                if (renderSphereLines)
+                {
+                    Instantiate(sphere, pos, Quaternion.identity).tag = "Sphere";
+                }
             }
         }
     }

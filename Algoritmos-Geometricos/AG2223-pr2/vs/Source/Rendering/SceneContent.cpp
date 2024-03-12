c367b1ddbd7a96a72371fc740dadc1edd1c3dbf2
Emilio Martínez Conchillo <124682801+emc00073@users.noreply.github.com>
2024-03-12 19:04:36 +0100

    Prac2 finished

--- Algoritmos-Geometricos/AG2223-pr2/vs/Source/Rendering/SceneContent.cpp
+++ Algoritmos-Geometricos/AG2223-pr2/vs/Source/Rendering/SceneContent.cpp
@@ -15,145 +15,10 @@
 
 void AlgGeom::SceneContent::buildScenario()
 {
-    /*
-    vec3 minBoundaries = vec3(-3.0f, -.4, -3.0f), maxBoundaries = vec3(-minBoundaries);
-
-    // Random segments
-    int numSegments = 8;
-
-    for (int segmentIdx = 0; segmentIdx < numSegments; ++segmentIdx)
-    {
-        Vect3d a(RandomUtilities::getUniformRandom(minBoundaries.x, maxBoundaries.x), RandomUtilities::getUniformRandom(minBoundaries.y, maxBoundaries.y), RandomUtilities::getUniformRandom(minBoundaries.z, maxBoundaries.z));
-        Vect3d b(RandomUtilities::getUniformRandom(minBoundaries.x, maxBoundaries.x), RandomUtilities::getUniformRandom(minBoundaries.y, maxBoundaries.y), RandomUtilities::getUniformRandom(minBoundaries.z, maxBoundaries.z));
-        Segment3d* segment = new Segment3d(a, b);
-
-        this->addNewModel((new DrawSegment(*segment))->setLineColor(RandomUtilities::getUniformRandomColor())->overrideModelName()->setLineWidth(RandomUtilities::getUniformRandom(1.0f, 3.0f)));
-        delete segment;
-    }
-
-    // Random points
-    int numPoints = 100;
-
-    for (int pointIdx = 0; pointIdx < numPoints; ++pointIdx)
-    {
-        Point point(RandomUtilities::getUniformRandom(minBoundaries.x, maxBoundaries.x), RandomUtilities::getUniformRandom(minBoundaries.x, maxBoundaries.x));
-        this->addNewModel((new DrawPoint(point))->setPointColor(RandomUtilities::getUniformRandomColor())->overrideModelName()->setPointSize(RandomUtilities::getUniformRandom(4.0f, 8.0f)));
-    }
-
-    // Random triangles
-    int numTriangles = 30;
-    float alpha = 2 * glm::pi<float>() / static_cast<float>(numTriangles);
-
-    for (int triangleIdx = 0; triangleIdx < numTriangles; ++triangleIdx)
-    {
-        float rand_b = RandomUtilities::getUniformRandom(.9f, 1.3f), rand_c = RandomUtilities::getUniformRandom(.8f, 1.4f);
-        Vect3d a(.0f, .0f, .0f);
-        Vect3d b(glm::cos(alpha * triangleIdx) * rand_b, glm::sin(alpha * triangleIdx) * rand_b, RandomUtilities::getUniformRandom(-.1f, .1f));
-        Vect3d c(glm::cos(alpha * (triangleIdx + 1)) * rand_c, glm::sin(alpha * (triangleIdx + 1)) * rand_c, RandomUtilities::getUniformRandom(-.1f, .1f));
-        Triangle3d* triangle = new Triangle3d(a, b, c);
-
-        this->addNewModel((new DrawTriangle(*triangle))->setLineColor(RandomUtilities::getUniformRandomColor())->setTriangleColor(vec4(RandomUtilities::getUniformRandomColor(), 1.0f))
-            ->overrideModelName());
-        delete triangle;
-    }
-
-    // Point cloud
-    PointCloud3d* pointCloud = new PointCloud3d(numPoints * 10, maxBoundaries.y * 6.0f);
-    this->addNewModel((new DrawPointCloud(*pointCloud))->setPointColor(RandomUtilities::getUniformRandomColor())->overrideModelName()->setPointSize(RandomUtilities::getUniformRandom(4.0f, 8.0f)));
-    delete pointCloud;
-
-    // Mesh
-    TriangleModel* triangleModel = new TriangleModel("Assets/Models/Ajax.obj");
-    auto model = (new DrawMesh(*triangleModel))->setModelMatrix(glm::translate(mat4(1.0f), vec3(.0f, .0f, .8f)))->overrideModelName();
-    this->addNewModel(model);
-
-    // AABB
-    AABB aabb(Vect3d(-1.0f, -2.0f, -1.5f), Vect3d(2.0f, 3.0f, 1.0f));
-    this->addNewModel((new DrawAABB(aabb))->overrideModelName());
-    */
-
     int numPoints = 50;
     vec3 minBoundaries = vec3(-3.0f, -.4, -3.0f), maxBoundaries = vec3(-minBoundaries);
 
-    if (false) {
-        // 2 Vect3d
-        Vect3d a(4.0f, -0.19939f, 3.0f), b(5.0f, 3.80061f, 5.0f);
-        Vect3d c(-0.93961f, 0.60697f, 5.0f), d(0.06039f, 1.60697f, 7.0f);
-
-        // 2 Line3d
-        Line3d* line1 = new Line3d(a, b);
-        Line3d* line2 = new Line3d(c, d);
-
-        // Mostrar las lineas
-        this->addNewModel((new DrawLine3d(*line1))->setLineColor(vec4(1.0f, .0f, .0f, 1.0f))->overrideModelName());
-        this->addNewModel((new DrawLine3d(*line2))->setLineColor(vec4(.0f, 1.0f, .0f, 1.0f))->overrideModelName());
-
-        // Distance between 2 lines
-        float distance = line1->distance(*line2);
-
-        std::cout << "Distance between 2 lines: " << distance << std::endl;
-
-        delete line1;
-        delete line2;
-    }
-
-    if (false) {
-        // 2 Vect3d
-        Vect3d a(4.0f, -0.19939f, 3.0f), b(5.0f, 3.80061f, 5.0f);
-        Vect3d p(-0.93961f, 0.60697f, 5.0f);
-
-        // 1 Line3d
-        Line3d* line = new Line3d(a, b);
-
-        // Mostrar la linea
-        this->addNewModel((new DrawLine3d(*line))->setLineColor(vec4(1.0f, .0f, .0f, 1.0f))->overrideModelName());
-        // Mostrar el punto
-        this->addNewModel((new DrawPoint(p))->setPointColor(vec4(1.0f, 1.0f, 1.0f, 1.0f))->setPointSize(3.0f)->overrideModelName());
-
-        // Line that contains the point normal to the first line
-        Line3d lineNormal = line->normalLine(p);
-
-        // Mostrar la linea
-        this->addNewModel((new DrawLine3d(lineNormal))->setLineColor(vec4(.0f, 1.0f, .0f, 1.0f))->overrideModelName());
-
-        delete line;
-    }
-
-    if (false) {
-        // Comprobar si dos lineas son paralelas o perpendiculares
-        Vect3d a(0.0f, 0.0f, 0.0f), b(1.0f, 0.0f, 0.0f);
-        Vect3d c(0.0f, 0.0f, 1.0f), d(1.0f, 0.0f, 1.0f);
-
-        // 2 Line3d
-        Line3d* line1 = new Line3d(a, b);
-        Line3d* line2 = new Line3d(c, d);
-
-        // Mostrar las lineas
-        this->addNewModel((new DrawLine3d(*line1))->setLineColor(vec4(1.0f, .0f, .0f, 1.0f))->overrideModelName());
-        this->addNewModel((new DrawLine3d(*line2))->setLineColor(vec4(.0f, 1.0f, .0f, 1.0f))->overrideModelName());
-
-        // Comprobar si son paralelas
-        bool areParallel = line1->classify(*line2) == Line3d::classifyLines::PARALLEL;
-
-        std::cout << "Are parallel: " << areParallel << std::endl;
-
-        // Comprobar si son perpendiculares
-        bool arePerpendicular = line1->classify(*line2) == Line3d::classifyLines::INTERSECT;
-
-        std::cout << "Are perpendicular: " << arePerpendicular << std::endl;
-    }
-
-    if (false) {
-        // Crear un plano con 3 Vect3d y dibujarlo
-        Vect3d a(0.0f, 0.0f, 0.0f), b(1.0f, 0.0f, 0.0f), c(0.0f, 1.0f, 0.0f);
-        Plane* plane = new Plane(a, b, c, true);
-
-        // Mostrar el plano
-        this->addNewModel((new DrawPlane(*plane))->setLineColor(vec4(1.0f, .0f, .0f, 1.0f))->overrideModelName());
-
-        delete plane;
-    }
-
+    char Pra2 = 'c';
 
     // Práctica 2a
     /*
@@ -173,7 +38,7 @@ void AlgGeom::SceneContent::buildScenario()
         8. Obtener el plano P, correspondiente a la tapa superior de la caja y
         pintarlo.
     */
-    if (false) {
+    if (Pra2 == 'a') {
         // 1. Una nube de puntos aleatoria de tamaño 50. Fijar un color para que
 		// todos los puntos de la nube tengan el mismo.
 		PointCloud3d* pointCloud = new PointCloud3d(numPoints, maxBoundaries.y * 6.0f);
@@ -301,7 +166,7 @@ void AlgGeom::SceneContent::buildScenario()
         A de V1.
         4. Comprobar que la distancia de V1 y V2 al plano A es la misma.
     */
-    if (true) {
+    if (Pra2 == 'b') {
         // 1. Crear el plano A con tres puntos aleatorios (plano vertical).
 
         Vect3d a = Vect3d(0.0f, 0.0f, -4.0f);
@@ -339,13 +204,13 @@ void AlgGeom::SceneContent::buildScenario()
 
         Vect3d v1 = Vect3d(-2.0f, 0.0f, -2.2f);
         Vect3d v__;
-        this->addNewModel((new DrawPoint(v1))->setPointColor(vec4(1.0f, .0f, 1.0f, 1.0f))->setPointSize(3.0f)->overrideModelName());
+        this->addNewModel((new DrawPoint(v1))->setPointColor(vec4(.0f, .0f, .0f, .0f))->setPointSize(3.0f)->overrideModelName());
 
         float distance = plane->distance(v1, v__);
         std::cout << "Distancia de V1 al plano A: " << distance << std::endl;
 
         Vect3d v2 = plane->reflectedPoint(v1);
-        this->addNewModel((new DrawPoint(v2))->setPointColor(vec4(1.0f, .0f, 1.0f, 1.0f))->setPointSize(3.0f)->overrideModelName());
+        this->addNewModel((new DrawPoint(v2))->setPointColor(vec4(.0f, .0f, .0f, .0f))->setPointSize(3.0f)->overrideModelName());
 
         // 4. Comprobar que la distancia de V1 y V2 al plano A es la misma.
 
@@ -365,11 +230,11 @@ void AlgGeom::SceneContent::buildScenario()
         normales se asemejen más a los 6 planos ortogonales (1,0,0), (0,1,0),
         (0,0,1), (-1,0,0), (0,-1,0), (0,0,-1).
     */
-    if (true) {
+    if (Pra2 == 'c') {
         // 1. Crea un objeto TriangleModel a partir de cualquier .obj y mostrarlo
         // en modo alambre.
         TriangleModel* triangleModel = new TriangleModel("Assets/Models/Dog.obj");
-        auto model = (new DrawMesh(*triangleModel))->setModelMatrix(glm::translate(mat4(1.0f), vec3(.0f, .0f, .8f)))->overrideModelName();
+        auto model = (new DrawMesh(*triangleModel))->overrideModelName();
         this->addNewModel(model);
 
         // 2. Dibujar con otro color los triángulos, en principio 6 distintos, cuyas
@@ -454,7 +319,7 @@ void AlgGeom::SceneContent::buildScenario()
 			}
         }
 
-        // Agregar los triángulos especiales al modelo
+        // Mostrar los triángulos especiales
         this->addNewModel((new DrawTriangle(triangle1))->setLineColor(vec4(1.0f, 1.0f, 1.0f, 1.0f))->setTriangleColor(vec4(1.0f, 0.0f, 0.0f, 1.0f))->overrideModelName());
         this->addNewModel((new DrawTriangle(triangle2))->setLineColor(vec4(1.0f, 1.0f, 1.0f, 1.0f))->setTriangleColor(vec4(0.0f, 1.0f, 0.0f, 1.0f))->overrideModelName());
         this->addNewModel((new DrawTriangle(triangle3))->setLineColor(vec4(1.0f, 1.0f, 1.0f, 1.0f))->setTriangleColor(vec4(0.0f, 0.0f, 1.0f, 1.0f))->overrideModelName());
